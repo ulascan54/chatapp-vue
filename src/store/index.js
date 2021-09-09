f973daecb73612dac473472cd1cfc4f25d3f0abf
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-09-09 13:07:07 +0300

    vuex store dosyası hazırlandı

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { store } from 'quasar/wrappers'
 import { createStore } from 'vuex'
 
-// import example from './module-example'
+import s from './store'
 
 /*
  * If not building with SSR mode, you can
@@ -15,7 +15,7 @@ import { createStore } from 'vuex'
 export default store(function (/* { ssrContext } */) {
   const Store = createStore({
     modules: {
-      // example
+      s
     },
 
     // enable strict mode (adds overhead!)
