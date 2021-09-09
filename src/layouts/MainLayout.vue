c98831813cbdf3067210718a91a3e523dd375237
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-09-09 11:58:22 +0300

    kullanıcılar sayfası tasarlandı sayfalar arası  route ayarı yapıldı

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,15 +2,24 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-
+        <q-btn
+          v-if="$route.fullPath.includes('/chat')"
+          @click="$router.go(-1)"
+          icon="arrow_back people_alt"
+          flat
+        />
+        <q-btn
+           to="/auth"
+          icon="account_circle login"
+          class="absolute-right q-pr-lg"
+          flat
+        />
 
         <q-toolbar-title class="absolute-center">
           {{ title }}
         </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
-
 
     <q-page-container>
       <router-view />
@@ -19,22 +28,20 @@
 </template>
 
 <script>
-import { openURL} from 'quasar'
+import { openURL } from "quasar";
 
 export default {
-
   computed: {
-    title(){
-      console.log(this.$route)
-      let currentPath = this.$route.fullPath
-      if (currentPath =='/') return 'ScreetChat'
-      else if(currentPath=='/chat') return 'Mesajlar'
-      else if (currentPath=='/auth') return 'Giriş'
-    }
+    title() {
+      console.log(this.$route);
+      let currentPath = this.$route.fullPath;
+      if (currentPath == "/") return "ScreetChat";
+      else if (currentPath == "/chat") return "Mesajlar";
+      else if (currentPath == "/auth") return "Giriş";
+    },
   },
-  methods:{
-    openURL
-  }
-
-}
+  methods: {
+    openURL,
+  },
+};
 </script>
--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -1,10 +1,78 @@
 <template>
-  <q-page class="flex">
-    <h5>Chat Page</h5>
+  <q-page class="flex column">
+    <q-banner inline-actions class="bg-grey-4 text-center">
+      <q-icon name="wifi_off" color="grey-8" :size="'20px'" class="q-mr-sm"/>
+      Kullanıcı şuan aktif değil !
+    </q-banner>
+    <div class="q-pa-md column col justify-end">
+      <q-chat-message
+        v-for="(message, index) in messages"
+        :key="index"
+        :name="message.from"
+        :text="[message.text]"
+        :sent="message.from === 'me' ? true : false"
+      />
+    </div>
+    <q-footer elevated>
+      <q-toolbar>
+        <q-form class="full-width">
+          <q-input
+            v-model="newMessage"
+            dark
+            outlined
+            rounded
+            dense
+            placeholder="Mesajınız"
+            color="white"
+            @keydown.enter="sendMessage"
+          >
+            <template v-slot:after>
+              <q-btn
+                @click="sendMessage"
+                round
+                dense
+                flat
+                icon="chat_bubble"
+                color="white"
+              ></q-btn>
+            </template>
+          </q-input>
+        </q-form>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
 <script>
-export default{
-}
+export default {
+  data() {
+    return {
+      newMessage: "",
+      messages: [
+        {
+          text: "Naber nasılsın",
+          from: "me",
+        },
+        {
+          text: "iyi knk sen",
+          from: "them",
+        },
+        {
+          text: "İyi ne olsun",
+          from: "me",
+        },
+      ],
+    };
+  },
+  methods: {
+    sendMessage() {
+      console.log(this.newMessage);
+      this.messages.push({
+        text: this.newMessage,
+        from: "me",
+      });
+      this.newMessage = "";
+    },
+  },
+};
 </script>
