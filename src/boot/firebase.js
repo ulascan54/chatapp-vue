dc0552065add47714352245b1cef6978fe996fa9
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2021-09-11 15:24:51 +0300

    Update firebase.js

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,12 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOVuO_wNIpmz5piB0ALG7TYvjgLJJIwEU",
-  authDomain: "screetchat-fa3a8.firebaseapp.com",
-  projectId: "screetchat-fa3a8",
-  storageBucket: "screetchat-fa3a8.appspot.com",
-  messagingSenderId: "636746985150",
-  appId: "1:636746985150:web:990cb5dd4737badcc378ef",
+//your config
 };
 
 let firebaseApp = firebase.initializeApp(firebaseConfig);
